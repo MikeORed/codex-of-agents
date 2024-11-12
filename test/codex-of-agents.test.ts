eac91d689c25b2c2788dc40ddd47961da64cce98
MikeORed <flanaganpic@gmail.com>
2024-11-12 13:36:04 -0600

    Initial commit

---
+++ test/codex-of-agents.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CodexOfAgents from '../lib/codex-of-agents-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/codex-of-agents-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CodexOfAgents.CodexOfAgentsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
